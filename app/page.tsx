f1b6cb91bb05cfe9de22e0c548459c4ee07709d1
Dorothy Baker <tbakerdorothy@gmail.com>
2024-01-21 20:38:30 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,27 @@
-import MainPage from "./components/MainPage";
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import SearchFeed from "./components/SearchFeed";
+import Foods from "././components/Foods";
 import Search from "./components/Search";
+import { Suspense } from "react";
+import Loading from "./components/Loading";
 
 export default function Home() {
+  const searchParams = useSearchParams();
+
+  const query = searchParams.has("query");
+  const category = searchParams.has("category");
+
   return (
     <div className="max-w-7xl mb-7 mx-auto">
-      <Search />
-      <MainPage />
+      <Suspense fallback={<Loading />}>
+        <Search />
+        <div className="border-t py-2">
+          {query && <SearchFeed />}
+          {category && <Foods />}
+        </div>
+      </Suspense>
     </div>
   );
 }
